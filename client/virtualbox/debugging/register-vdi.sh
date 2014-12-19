c2399fa50ef223fe11d211ef4da9c26683806922
RyanWBunney <rwbunney@gmail.com>
2014-12-19 18:50:39 +0800

    Added setup files/debug files/server templates

---
+++ client/virtualbox/debugging/register-vdi.sh
@@ -0,0 +1,10 @@
+#Shell script to register a testing VM
+
+vboxmanage createvm --name "testvm" --register
+vboxmanage modicfy vm "testvm" --memory 512 --acpi on --boot1 dvd
+vboxmanage modifyvm "testvm" --memory 512 --acpi on --boot1 dvd
+vboxmanage modifyvm "testvm" --nic1 bridged --bridgeadapter1 eth0
+vboxmanage modifyvm "testvm" --ostype Debian_64
+vboxmanage storagectl "testvm" --name "IDE Controller" --add ide
+vboxmanage storageattach "testvm" --storagectl "IDE Controller" --port 1 --device 0 --type hdd --medium ./vm_image.vdi
+vboxmanage sharedfolder add "testvm" --name shared --hostpath /full/working/directory/name --automount
