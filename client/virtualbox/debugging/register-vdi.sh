85665b839dae084e87d92a6e6292cf3342170bcb
RyanWBunney <rwbunney@gmail.com>
2015-01-19 10:11:28 +0800

    Update of some template files

--- client/virtualbox/debugging/register-vdi.sh
+++ client/virtualbox/debugging/register-vdi.sh
@@ -1,5 +1,5 @@
 #Shell script to register a testing VM
-
+#TODO Change memory down to 256
 vboxmanage createvm --name "testvm" --register
 vboxmanage modicfy vm "testvm" --memory 512 --acpi on --boot1 dvd
 vboxmanage modifyvm "testvm" --memory 512 --acpi on --boot1 dvd
