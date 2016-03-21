8f4a74ab73836e00a3933ca730dd26b99efafb12
sam6321 <sam6321@live.com.au>
2016-03-21 10:24:07 +0800

    Config file now will print out config when run. Added all config options to the config file.

--- server/config/__init__.py
+++ server/config/__init__.py
@@ -1,9 +1,11 @@
 """
-The very small configuration file!
+Configuration file for sourcefinder.
+Run this file directly to print the configuration.
+
+!!IF YOU MAKE A CHANGE HERE, MAKE SURE make_default.py IS CHANGED TOO!!
 """
 from os.path import exists, dirname
 from configobj import ConfigObj
-
 
 config_file_name = dirname(__file__) + '/duchamp.settings'
 if exists(config_file_name):
@@ -16,3 +18,19 @@ if exists(config_file_name):
     BOINC_DB_NAME = config['boincDatabaseName']
     DB_LOGIN = "mysql://" + DB_USER_ID + ":" + DB_PASSWORD + "@" + DB_HOSTNAME + "/" + DB_NAME
     BOINC_DB_LOGIN = "mysql://" + DB_USER_ID + ":" + DB_PASSWORD + "@" + DB_HOSTNAME + "/" + BOINC_DB_NAME
+
+    ############### Directory Settings ###############
+    DIR_PARAM = config['paramDirectory']
+    DIR_CUBE = config['cubeDirectory']
+    DIR_BOINC_PROJECT_PATH = config['boincPath']
+
+    ############### Work Generation Settings ###############
+    WG_THRESHOLD = config['wgThreshold']
+
+if __name__ == '__main__':
+    # Print the config
+    if not config:
+        print 'No config file'
+    else:
+        for k, v in config.iteritems():
+            print '{0}: {1}'.format(k, v)
