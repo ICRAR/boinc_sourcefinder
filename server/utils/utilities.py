e433d508ff1b999820b602fbbc036cad620e42af
sam6321 <sam6321@live.com.au>
2016-07-12 11:35:26 +0800

    Should be deleting

--- server/utils/utilities.py
+++ server/utils/utilities.py
@@ -1,1 +1,11 @@
 # A generic utilities package containing misc functions and classes used by server apps.
+
+
+def retry_on_exception(function, exception, num_retries):
+
+    while num_retries > 0:
+        try:
+            function()
+            num_retries = 0
+        except exception:
+            num_retries -= 1
