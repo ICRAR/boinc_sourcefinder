fd43aa0a1ca2a23f4908149ffa0dc7c9971f4b0d
sam6321 <sam6321@live.com.au>
2016-08-16 14:38:37 +0800

    Fixing up VM app creation

--- server/workgeneration/register_run.py
+++ server/workgeneration/register_run.py
@@ -144,8 +144,8 @@ def register_parameters_runid(run_id, parameters):
         ret = connection.execute(select([PARAMETER_FILE]))
         for row in ret:
 
-            if not int(row['parameter_file_id']) in exists:  # only add this to the DB if it does not already exist
-                connection.execute(PARAMETER_RUN.insert(), parameter_id=int(row['parameter_file_id']), run_id=run_id)
+            if not int(row['parameter_id']) in exists:  # only add this to the DB if it does not already exist
+                connection.execute(PARAMETER_RUN.insert(), parameter_id=int(row['parameter_id']), run_id=run_id)
     else:
 
         # Silently remove all invalids from the list.
