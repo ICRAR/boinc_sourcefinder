3509935ca51230c3272ed81b19802889fe2c2845
sam6321 <sam6321@live.com.au>
2016-09-06 11:08:02 +0800

    Fixed path error in assimilator

--- server/assimilator/sourcefinder_assimilator.py
+++ server/assimilator/sourcefinder_assimilator.py
@@ -5,8 +5,7 @@ import sys
 
 # Setup the Python Path as we may be running this via ssh
 base_path = os.path.dirname(__file__)
-sys.path.append(os.path.abspath(os.path.join(base_path, '..')))
-sys.path.append(os.path.abspath(os.path.join(base_path, '../py')))
+sys.path.append(os.path.abspath(os.path.join(base_path, '/home/ec2-user/projects/duchamp/py')))
 sys.path.append('/home/ec2-user/boinc_sourcefinder/server')
 sys.path.append('/home/ec2-user/boinc_sourcefinder/server/assimilator')
 
