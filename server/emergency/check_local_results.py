d127ab9a2562b2254af3b431d2ce816d8fc36ad3
sam6321 <sam6321@live.com.au>
2017-06-14 10:06:41 +0800

    Added try-finally to

--- server/emergency/check_local_results.py
+++ server/emergency/check_local_results.py
@@ -108,7 +108,7 @@ if __name__ == '__main__':
 
     print "file_names"
     for key in file_names:
-        print key, len(file_names[key])
+        print key, len(files_index[key])
 
     print "We have: "
     print len(ones_we_have)
