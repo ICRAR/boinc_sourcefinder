99d79227e07daea2c6b8f92386486a9e562a41f8
sam6321 <sam6321@live.com.au>
2017-06-14 15:06:41 +0800

    Added try-finally to

--- server/assimilator/sourcefinder_assimilator.py
+++ server/assimilator/sourcefinder_assimilator.py
@@ -87,7 +87,7 @@ class SourcefinderAssimilator(assimilator.Assimilator):
             underscore = wu.name.find('_')
             res = self.engine.execute(select([CUBE]).where(CUBE.c.cube_name == wu.name[underscore + 1:])).first()
 
-            if res is None or res[0]['progress'] == 2:
+            if res is None or res['progress'] == 2:
                 self.logCritical("Skipping because it's already been processed\n")
                 continue
 
