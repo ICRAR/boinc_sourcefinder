0d5bcc663688f52e26b6b4716304181c31f02ceb
sam6321 <sam6321@live.com.au>
2017-08-30 14:56:31 +0800

    Fixing path issue with validator script

--- server/duchamp/validator_duchamp_init.sh
+++ server/duchamp/validator_duchamp_init.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 # Forwards arguments from the BOINC script_validator to validator.py
-SCRIPT_PATH=$( cd $(dirname $0) ; pwd -P )
-$SCRIPT_PATH/../validator.py init --app duchamp ${@:1}
+/home/ec2-user/boinc_sourcefinder/server/validator.py init --app duchamp ${@:1}
 exit $?
--- server/sofia/validator_sofia_compare.sh
+++ server/sofia/validator_sofia_compare.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 # Forwards arguments from the BOINC script_validator to validator.py
-SCRIPT_PATH=$( cd $(dirname $0) ; pwd -P )
-${SCRIPT_PATH}/../validator.py compare --app sofia ${@:1}
+/home/ec2-user/boinc_sourcefinder/server/validator.py compare --app sofia ${@:1}
 exit $?
