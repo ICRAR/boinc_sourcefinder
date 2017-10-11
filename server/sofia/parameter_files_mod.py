f460cfacecbe97b8d65d9df10112b7ad2ac5a2ba
sam6321 <sam6321@live.com.au>
2017-10-11 12:34:33 +0800

    Restructuring parameter file generation slightly to allow multiple lines to be generated from one config item.

--- server/sofia/parameter_files_mod.py
+++ server/sofia/parameter_files_mod.py
@@ -34,8 +34,8 @@ OUTPUT_DIR = "/root/shared/output/"
 
 
 class ConfigItem:
-    def __init__(self, name, possible_values):
-        self.name = name
+    def __init__(self, format, possible_values):
+        self.format = format
 
         if type(possible_values) != list:
             possible_values = [possible_values]
@@ -52,119 +52,115 @@ class ConfigItem:
         return iter(self.possible_values)
 
 SOFIA_CONFIG = [
-    ConfigItem("steps.doSubcube", "false"),
-    ConfigItem("steps.doFlag", "false"),
-    ConfigItem("steps.doSmooth", "false"),
-    ConfigItem("steps.doScaleNoise", "false"),
-    ConfigItem("steps.doSCfind", "true"),
-    ConfigItem("steps.doThreshold", "false"),
-    ConfigItem("steps.doWavelet", "false"),
-    ConfigItem("steps.doCNHI", "false"),
-    ConfigItem("steps.doMerge", "true"),
-    ConfigItem("steps.doReliability", "true"),
-    ConfigItem("steps.doParameterise", "true"),
-    ConfigItem("steps.doWriteFilteredCube", "false"),
-    ConfigItem("steps.doWriteMask", "false"),
-    ConfigItem("steps.doWriteCat", "true"),
-    ConfigItem("steps.doMom0", "false"),
-    ConfigItem("steps.doMom1", "false"),
-    ConfigItem("steps.doCubelets", "false"),
-    ConfigItem("steps.doDebug", "false"),
-    ConfigItem("steps.doOptical", "false"),
+    ConfigItem("steps.doSubcube = {0}", "false"),
+    ConfigItem("steps.doFlag = {0}", "false"),
+    ConfigItem("steps.doSmooth = {0}", "false"),
+    ConfigItem("steps.doScaleNoise = {0}", "false"),
+    ConfigItem("steps.doSCfind = {0}", "true"),
+    ConfigItem("steps.doThreshold = {0}", "false"),
+    ConfigItem("steps.doWavelet = {0}", "false"),
+    ConfigItem("steps.doCNHI = {0}", "false"),
+    ConfigItem("steps.doMerge = {0}", "true"),
+    ConfigItem("steps.doReliability = {0}", "true"),
+    ConfigItem("steps.doParameterise = {0}", "true"),
+    ConfigItem("steps.doWriteFilteredCube = {0}", "false"),
+    ConfigItem("steps.doWriteMask = {0}", "false"),
+    ConfigItem("steps.doWriteCat = {0}", "true"),
+    ConfigItem("steps.doMom0 = {0}", "false"),
+    ConfigItem("steps.doMom1 = {0}", "false"),
+    ConfigItem("steps.doCubelets = {0}", "false"),
+    ConfigItem("steps.doDebug = {0}", "false"),
+    ConfigItem("steps.doOptical = {0}", "false"),
 
-    ConfigItem("import.inFile", INPUT_FILE),
-    ConfigItem("import.weightsFile", ""),
-    ConfigItem("import.maskFile", ""),
-    ConfigItem("import.weightsFunction", ""),
-    ConfigItem("import.subcube", "[]"),
-    ConfigItem("import.subcubeMode", "pixel"),
+    ConfigItem("import.inFile = {0}", INPUT_FILE),
+    ConfigItem("import.weightsFile = {0}", ""),
+    ConfigItem("import.maskFile = {0}", ""),
+    ConfigItem("import.weightsFunction = {0}", ""),
+    ConfigItem("import.subcube = {0}", "[]"),
+    ConfigItem("import.subcubeMode = {0}", "pixel"),
 
-    ConfigItem("flag.regions", "[]"),
+    ConfigItem("flag.regions = {0}", "[]"),
 
-    ConfigItem("optical.sourceCatalogue", ""),
-    ConfigItem("optical.spatSize", "0.01"),
-    ConfigItem("optical.specSize", "1e+5"),
-    ConfigItem("optical.storeMultiCat", "false"),
+    ConfigItem("optical.sourceCatalogue = {0}", ""),
+    ConfigItem("optical.spatSize = {0}", "0.01"),
+    ConfigItem("optical.specSize = {0}", "1e+5"),
+    ConfigItem("optical.storeMultiCat = {0}", "false"),
 
-    ConfigItem("smooth.kernel", "gaussian"),
-    ConfigItem("smooth.edgeMode", "constant"),
-    ConfigItem("smooth.kernelX", "3.0"),
-    ConfigItem("smooth.kernelY", "3.0"),
-    ConfigItem("smooth.kernelZ", "3.0"),
+    ConfigItem("smooth.kernel = {0}", "gaussian"),
+    ConfigItem("smooth.edgeMode = {0}", "constant"),
+    ConfigItem("smooth.kernelX = {0}", "3.0"),
+    ConfigItem("smooth.kernelY = {0}", "3.0"),
+    ConfigItem("smooth.kernelZ = {0}", "3.0"),
 
-    ConfigItem("scaleNoise.statistic", "mad"),
-    ConfigItem("scaleNoise.scaleX", "false"),
-    ConfigItem("scaleNoise.scaleY", "false"),
-    ConfigItem("scaleNoise.scaleZ", "true"),
-    ConfigItem("scaleNoise.edgeX", "0"),
-    ConfigItem("scaleNoise.edgeY", "0"),
-    ConfigItem("scaleNoise.edgeZ", "0"),
+    ConfigItem("scaleNoise.statistic = {0}", "mad"),
+    ConfigItem("scaleNoise.scaleX = {0}", "false"),
+    ConfigItem("scaleNoise.scaleY = {0}", "false"),
+    ConfigItem("scaleNoise.scaleZ = {0}", "true"),
+    ConfigItem("scaleNoise.edgeX = {0}", "0"),
+    ConfigItem("scaleNoise.edgeY = {0}", "0"),
+    ConfigItem("scaleNoise.edgeZ = {0}", "0"),
 
-    ConfigItem("wavelet.threshold", "5.0"),
-    ConfigItem("wavelet.scaleXY", "-1"),
-    ConfigItem("wavelet.scaleZ", "-1"),
-    ConfigItem("wavelet.positivity", "false"),
-    ConfigItem("wavelet.iterations", "3"),
+    ConfigItem("wavelet.threshold = {0}", "5.0"),
+    ConfigItem("wavelet.scaleXY = {0}", "-1"),
+    ConfigItem("wavelet.scaleZ = {0}", "-1"),
+    ConfigItem("wavelet.positivity = {0}", "false"),
+    ConfigItem("wavelet.iterations = {0}", "3"),
 
-    ConfigItem("SCfind.threshold", ["3.0", "4.0", "5.0"]),
-    ConfigItem("SCfind.sizeFilter", "0.0"),
-    ConfigItem("SCfind.maskScaleXY", "2.0"),
-    ConfigItem("SCfind.maskScaleZ", "2.0"),
-    ConfigItem("SCfind.edgeMode", "constant"),
-    ConfigItem("SCfind.rmsMode", "negative"),
-    ConfigItem("SCfind.kernels", ["[[0, 0, 0, 'b'], [0, 0, 3, 'b'], [0, 0, 7, 'b'], [0, 0, 15, 'b'], [3, 3, 0, 'b'], [3, 3, 3, 'b'], [3, 3, 7, 'b'], [3, 3, 15, 'b'], [6, 6, 0, 'b'], [6, 6, 3, 'b'], [6, 6, 7, 'b'], [6, 6, 15, 'b']]",
+    ConfigItem("SCfind.threshold = {0}", ["3.0", "4.0", "5.0"]),
+    ConfigItem("SCfind.sizeFilter = {0}", "0.0"),
+    ConfigItem("SCfind.maskScaleXY = {0}", "2.0"),
+    ConfigItem("SCfind.maskScaleZ = {0}", "2.0"),
+    ConfigItem("SCfind.edgeMode = {0}", "constant"),
+    ConfigItem("SCfind.rmsMode = {0}", "negative"),
+    ConfigItem("SCfind.kernels = {0}", ["[[0, 0, 0, 'b'], [0, 0, 3, 'b'], [0, 0, 7, 'b'], [0, 0, 15, 'b'], [3, 3, 0, 'b'], [3, 3, 3, 'b'], [3, 3, 7, 'b'], [3, 3, 15, 'b'], [6, 6, 0, 'b'], [6, 6, 3, 'b'], [6, 6, 7, 'b'], [6, 6, 15, 'b']]",
                                   "[[0, 0, 0, 'b'], [0, 0, 3, 'b'], [0, 0, 7, 'b'], [0, 0, 15, 'b'], [0, 0, 31, 'b'], [0, 0, 63, 'b'], [3, 3, 0, 'b'], [3, 3, 3, 'b'], [3, 3, 7, 'b'], [3, 3, 15, 'b'], [3, 3, 31, 'b'], [3, 3, 63, 'b']]",
                                   "[[0, 0, 0, 'b'], [0, 0, 3, 'b'], [0, 0, 7, 'b'], [3, 3, 0, 'b'], [3, 3, 3, 'b'], [3, 3, 7, 'b'], [6, 6, 0, 'b'], [6, 6, 3, 'b'], [6, 6, 7, 'b'], [10, 10, 0, 'b'], [10, 10, 3, 'b'], [10, 10, 7, 'b']]"]),
-    ConfigItem("SCfind.kernelUnit", "pixel"),
-    ConfigItem("SCfind.verbose", "true"),
+    ConfigItem("SCfind.kernelUnit = {0}", "pixel"),
+    ConfigItem("SCfind.verbose = {0}", "true"),
 
-    ConfigItem("CNHI.pReq", "1e-5"),
-    ConfigItem("CNHI.qReq", "3.8"),
-    ConfigItem("CNHI.minScale", "5"),
-    ConfigItem("CNHI.maxScale", "-1"),
-    ConfigItem("CNHI.medianTest", "true"),
-    ConfigItem("CNHI.verbose", "1"),
+    ConfigItem("CNHI.pReq = {0}", "1e-5"),
+    ConfigItem("CNHI.qReq = {0}", "3.8"),
+    ConfigItem("CNHI.minScale = {0}", "5"),
+    ConfigItem("CNHI.maxScale = {0}", "-1"),
+    ConfigItem("CNHI.medianTest = {0}", "true"),
+    ConfigItem("CNHI.verbose = {0}", "1"),
 
-    ConfigItem("threshold.threshold", "4.0"),
-    ConfigItem("threshold.clipMethod", "relative"),
-    ConfigItem("threshold.rmsMode", "std"),
-    ConfigItem("threshold.verbose", "false"),
+    ConfigItem("threshold.threshold = {0}", "4.0"),
+    ConfigItem("threshold.clipMethod = {0}", "relative"),
+    ConfigItem("threshold.rmsMode = {0}", "std"),
+    ConfigItem("threshold.verbose = {0}", "false"),
 
-    ConfigItem("merge.radiusX", ["1", "3", "5"]),
-    ConfigItem("merge.radiusY", ["1", "3", "5"]),
-    ConfigItem("merge.radiusZ", ["1", "3", "5"]),
-    ConfigItem("merge.minSizeX", ["3", "5", "10"]),
-    ConfigItem("merge.minSizeY", ["3", "5", "10"]),
-    ConfigItem("merge.minSizeZ", ["3", "5", "10"]),
-    ConfigItem("merge.positivity", "false"),
+    ConfigItem("merge.radiusX = {0}\nmerge.radiusY = {0}\nmerge.radiusZ = {0}", ["1", "3", "5"]),
+    ConfigItem("merge.minSizeX = {0}\nmerge.minSizeY = {0}\nmerge.minSizeZ = {0}", ["3", "5", "10"]),
+    ConfigItem("merge.positivity = {0}", "false"),
 
-    ConfigItem("parameters.fitBusyFunction", "false"),
-    ConfigItem("parameters.optimiseMask", "false"),
-    ConfigItem("parameters.dilateMask", ["true", "false"]),
-    ConfigItem("parameters.dilateThreshold", "0.02"),
-    ConfigItem("parameters.dilatePixMax", "10"),
-    ConfigItem("parameters.dilateChan", "1"),
+    ConfigItem("parameters.fitBusyFunction = {0}", "false"),
+    ConfigItem("parameters.optimiseMask = {0}", "false"),
+    ConfigItem("parameters.dilateMask = {0}", ["true", "false"]),
+    ConfigItem("parameters.dilateThreshold = {0}", "0.02"),
+    ConfigItem("parameters.dilatePixMax = {0}", "10"),
+    ConfigItem("parameters.dilateChan = {0}", "1"),
 
-    ConfigItem("reliability.parSpace", "['n_pix','snr_sum','snr_max']"),
-    ConfigItem("reliability.logPars", "[1,1,1]"),
-    ConfigItem("reliability.autoKernel", "true"),
-    ConfigItem("reliability.scaleKernel", ["0.4", "0.5", "0.6"]),
-    ConfigItem("reliability.usecov", "true"),
-    ConfigItem("reliability.negPerBin", "1.0"),
-    ConfigItem("reliability.skellamTol", "-0.5"),
-    ConfigItem("reliability.kernel", "[0.15,0.05,0.1]"),
-    ConfigItem("reliability.fMin", "10.0"),
-    ConfigItem("reliability.threshold", "0.5"),
-    ConfigItem("reliability.makePlot", "false"),
+    ConfigItem("reliability.parSpace = {0}", "['n_pix','snr_sum','snr_max']"),
+    ConfigItem("reliability.logPars = {0}", "[1,1,1]"),
+    ConfigItem("reliability.autoKernel = {0}", "true"),
+    ConfigItem("reliability.scaleKernel = {0}", ["0.4", "0.5", "0.6"]),
+    ConfigItem("reliability.usecov = {0}", "true"),
+    ConfigItem("reliability.negPerBin = {0}", "1.0"),
+    ConfigItem("reliability.skellamTol = {0}", "-0.5"),
+    ConfigItem("reliability.kernel = {0}", "[0.15,0.05,0.1]"),
+    ConfigItem("reliability.fMin = {0}", "10.0"),
+    ConfigItem("reliability.threshold = {0}", "0.5"),
+    ConfigItem("reliability.makePlot = {0}", "false"),
 
-    ConfigItem("writeCat.overwrite", "true"),
-    ConfigItem("writeCat.compress", "false"),
-    ConfigItem("writeCat.outputDir", OUTPUT_DIR),
-    ConfigItem("writeCat.basename", "sofia_output_{0}"),
-    ConfigItem("writeCat.writeASCII", "false"),
-    ConfigItem("writeCat.writeXML", "true"),
-    ConfigItem("writeCat.writeSQL", "false"),
-    ConfigItem("writeCat.parameters", "['*']")
+    ConfigItem("writeCat.overwrite = {0}", "true"),
+    ConfigItem("writeCat.compress = {0}", "false"),
+    ConfigItem("writeCat.outputDir = {0}", OUTPUT_DIR),
+    ConfigItem("writeCat.basename = {0}", "sofia_output_{0}"),
+    ConfigItem("writeCat.writeASCII = {0}", "false"),
+    ConfigItem("writeCat.writeXML = {0}", "true"),
+    ConfigItem("writeCat.writeSQL = {0}", "false"),
+    ConfigItem("writeCat.parameters = {0}", "['*']")
 ]
 
 
@@ -173,27 +169,23 @@ def get_parameter_file_generator(base_class):
     class SofiaParameterFileGenerator(base_class):
 
         def create_parameter_file_data(self):
-            file_count = 0
-
             items = []
 
-            for config in itertools.product(*SOFIA_CONFIG):
+            for file_count, config in enumerate(itertools.product(*SOFIA_CONFIG)):
                 filename = 'supercube_run_{0}_sofia.par'.format(file_count)
                 config_string = "# SoFiA Config for sourcefinder: {0}\n".format(filename)
 
                 for idx, field in enumerate(config):
-                    field_name = SOFIA_CONFIG[idx].name
+                    field_format = SOFIA_CONFIG[idx].name
                     output = field
 
-                    if field_name == "writeCat.basename":
+                    if field_format.startswith("writeCat.basename"):
                         # Set an incrementing number for the output file name.
                         output = field.format(file_count)
 
-                    config_string += "{0}   =   {1}\n".format(SOFIA_CONFIG[idx].name, output)
+                    config_string += "{0}\n".format(SOFIA_CONFIG[idx].format.format(output))
 
                 items.append((filename, config_string))
-
-                file_count += 1
 
             return items
 
